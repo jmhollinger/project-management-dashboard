a0e1c45f87f8ca45b8f6a0da776917a52a675dea
Jonathan Hollinger <jmholl5@gmail.com>
2015-08-05 17:02:22 -0400

    Update

--- js/map.js
+++ js/map.js
@@ -44,36 +44,80 @@ function initialize(){
     }};
 });
 
-  map.data.loadGeoJson('data/projects.geojson')
+map.data.loadGeoJson('data/projects.geojson')
 
+var infowindow = new google.maps.InfoWindow();
 
+map.data.addListener('click', function(event) {
 
-  var infowindow = new google.maps.InfoWindow();
+ShowProject(event.feature.getProperty('join_id'))  
 
-  map.data.addListener('click', function(event) {
-      $('#myModal').modal('show');
+  })
 
-      if (event.feature.getProperty('Status') === 'Progressing') {var status_button = "<p class=\"status good-status\">Project is Progressing</p>"}
-      else if (event.feature.getProperty('Status') === 'Stalled') {var status_button = "<p class=\"status warning-status\">Project is Stalled</p>"}
-      else if (event.feature.getProperty('Status') === 'Stopped') {var status_button = "<p class=\"status bad-status\">Project is Stopped</p>"}
-      else {}  
+  };
 
-      $("#myModalLabel").html("<h3>" + event.feature.getProperty('Project') + "</h3>" +
-        "<p>" + event.feature.getProperty('Description') + "</p>")
+google.maps.event.addDomListener(window, 'load', initialize);
 
-      $("#project").html(
+function FormatCurrency (input){ 
+if (input && $.isNumeric(input) === true) {
+var rounded = Math.round(input)
+return '$' + rounded.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+} else {return '$0'}};
+
+function DaysIn (StartDate){
+return Math.round(Math.abs(new Date() - new Date(StartDate))/86400000)
+}
+
+function TotalDays (StartDate, EndDate){
+return Math.round(Math.abs(new Date(EndDate) - new Date(StartDate))/86400000)
+}
+
+$(document).ready(function(){
+$.ajax({
+    url: 'data/projects.geojson',
+    dataType: 'json',
+    success: function(data) {
+    $.each(data.features, function(key, value){
+      if (value.properties.Status === 'Progressing') {var status_button = "<span class=\"status good-status\">Progressing</span>"}
+      else if (value.properties.Status === 'Stalled') {var status_button = "<span class=\"status warning-status\">Stalled</span>"}
+      else if (value.properties.Status === 'Stopped') {var status_button = "<span class=\"status bad-status\">Stopped</span>"}
+      else {}
+    $("#project-tbody").append("<tr style=\"cursor:pointer\" onclick=\"ShowProject(" + value.properties.join_id + ")\"><td><span class=\"project-name\">" + value.properties.Project + "</span></td><td>" + status_button + "</td></tr>")
+  })
+
+    }});
+})
+
+function ShowProject (join_id) {
+$.ajax({
+    url: 'data/projects.geojson',
+    dataType: 'json',
+    success: function(data) {
+    $.each(data.features, function(key, value){
+    if (value.properties.join_id === join_id){  
+
+    if (value.properties.Status === 'Progressing') {var status_button = "<p class=\"status good-status\">Project is Progressing</p>"}
+      else if (value.properties.Status === 'Stalled') {var status_button = "<p class=\"status warning-status\">Project is Stalled</p>"}
+      else if (value.properties.Status === 'Stopped') {var status_button = "<p class=\"status bad-status\">Project is Stopped</p>"}
+      else {}    
+    
+
+    $("#myModalLabel").html(
+      "<h3>" + value.properties.Project + "</h3>" +
+      "<p>" + value.properties.Description + "</p>")
+
+    $("#project").html(
         status_button +
-        "<p class=\"tight\"><strong>Start Date: </strong>" + event.feature.getProperty('StartDate') + "</p>" +
-        "<p class=\"tight\"><strong>End Date: </strong>" + event.feature.getProperty('EndDate') + "</p>" +
-        "<p><strong>Project Budget: </strong>" + FormatCurrency(event.feature.getProperty('Budget')) + "</p>" +       
-        "<p><strong>Current Task: </strong>" + event.feature.getProperty('Current') + 
-        "<br>This task is due on " + event.feature.getProperty('CurrentDueDate') + "</p>" +
-        "<p><strong>Last Task Completed: </strong>" + event.feature.getProperty('LastTask') +        
-        "<br>This task was completed on " + event.feature.getProperty('LastTaskDate') + "</p>" +
+        "<p class=\"tight\"><strong>Start Date: </strong>" + value.properties.StartDate + "</p>" +
+        "<p class=\"tight\"><strong>End Date: </strong>" + value.properties.EndDate + "</p>" +
+        "<p><strong>Project Budget: </strong>" + FormatCurrency(value.properties.Budget) + "</p>" +       
+        "<p><strong>Current Task: </strong>" + value.properties.Current + 
+        "<br>This task is due on " + value.properties.CurrentDueDate + "</p>" +
+        "<p><strong>Last Task Completed: </strong>" + value.properties.LastTask +        
+        "<br>This task was completed on " + value.properties.LastTaskDate + "</p>" +
         "<div class=\"row\"><div class=\"col-sm-6 chart\" id=\"budget-chart\"></div>" +
         "<div class=\"col-sm-6 chart\" id=\"time-chart\"></div></div>"
                 );
-
       var budgetchart = c3.generate({
         padding: {
           top: 10,
@@ -89,8 +133,8 @@ function initialize(){
                 }, 
        data: {
          columns: [
-          ['Expense', event.feature.getProperty('Expense')],
-          ['Available', event.feature.getProperty('Budget') - event.feature.getProperty('Expense')]
+          ['Expense', value.properties.Expense],
+          ['Available', value.properties.Budget - value.properties.Expense]
                 ],
          type : 'donut',
          colors: {
@@ -124,8 +168,8 @@ function initialize(){
                 }, 
        data: {
          columns: [
-          ['Elapsed', DaysIn(event.feature.getProperty('StartDate'))],
-          ['Remaining', TotalDays(event.feature.getProperty('StartDate'), event.feature.getProperty('EndDate')) - DaysIn(event.feature.getProperty('StartDate'))]
+          ['Elapsed', DaysIn(value.properties.StartDate)],
+          ['Remaining', TotalDays(value.properties.StartDate, value.properties.EndDate) - DaysIn(value.properties.StartDate)]
                 ],
          type : 'donut',
          colors: {
@@ -143,43 +187,15 @@ function initialize(){
                 },
         legend: {hide: true}
         });
+
+
+    $('#myModal').modal('show');
+    }
+
+else {}
+
   })
 
-  };
+    }})
 
-google.maps.event.addDomListener(window, 'load', initialize);
-
-function FormatCurrency (input){ 
-if (input && $.isNumeric(input) === true) {
-var rounded = Math.round(input)
-return '$' + rounded.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-} else {return '$0'}};
-
-function DaysIn (StartDate){
-return Math.round(Math.abs(new Date() - new Date(StartDate))/86400000)
 }
-
-function TotalDays (StartDate, EndDate){
-return Math.round(Math.abs(new Date(EndDate) - new Date(StartDate))/86400000)
-}
-
-$("#close-button").click(function(){
-  console.log("Close!")
-})
-
-$(document).ready(function(){
-$.ajax({
-    url: 'data/projects.geojson',
-    dataType: 'json',
-    success: function(data) {
-    $.each(data.features, function(key, value){
-      if (value.properties.Status === 'Progressing') {var status_button = "<p class=\"status good-status\">Progressing</p>"}
-      else if (value.properties.Status === 'Stalled') {var status_button = "<p class=\"status warning-status\">Stalled</p>"}
-      else if (value.properties.Status === 'Stopped') {var status_button = "<p class=\"status bad-status\">Stopped</p>"}
-      else {}
-
-    $("#project-tbody").append("<tr><td><p class=\"project-name\">" + value.properties.Project + "<p></td><td>" + status_button + "</td></tr>")
-  })
-
-    }});
-})
